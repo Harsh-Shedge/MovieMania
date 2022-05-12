19aa40b3fb0dbb7690cc7321b798f6e4d1933e91
Harsh-Shedge <88962612+Harsh-Shedge@users.noreply.github.com>
2022-05-12 12:03:30 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
 function App() {
+  const API_URL = "https://www.omdbapi.com?apikey=66278e14";
+
+  const [movies, setMovies] = useState([]);
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchMovies = async (title) => {
+    try {
+      const response = await fetch(`${API_URL}&s=${title}`);
+
+      const data = await response.json();
+      setMovies(data.Search);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        <h1>MovieMania</h1>
+
+        <div className="search">
+          <input
+            placeholder="Search for movies"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <img
+            src={SearchIcon}
+            alt="search"
+            onClick={() => searchMovies(searchTerm)}
+          />
+        </div>
+
+        {movies?.length > 0 ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie1={movie} />
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>Mo movies Found</h2>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
